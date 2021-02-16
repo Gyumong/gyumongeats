82b770006c33c717f904227b137e51e03f5448a1
Jiho Park <jiho5993@naver.com>
2021-02-16 16:36:31 +0900

    쿼리별 음식점 불러오는 API 작성 (별점순, 주문순 등)

--- back/routes/api/restaurant/restaurant.contoller.js
+++ back/routes/api/restaurant/restaurant.contoller.js
@@ -1,9 +1,37 @@
 const sequelize = require('sequelize');
+const { Op } = sequelize;
 const { Restaurant } = require('../../../models');
 
 exports.allRestaurants = async (req, res) => {
+  const type = req.query.t;
+  const fee = req.query.f;
+  const min_price = req.query.m;
+
   try {
-    const rest = await Restaurant.findAll({ order: sequelize.fn('RAND') });
+    let rest;
+    const cond = [];
+
+    if(fee) cond.push({ deliveryFee: { [Op.lte]: fee } });
+    if(min_price) cond.push({ minOrderPrice: { [Op.lte]: min_price } });
+
+    if(type === undefined) {
+      rest = await Restaurant.findAll({
+        where: {
+          [Op.and]: cond
+        },
+        order: sequelize.fn('RAND')
+      });
+    } else {
+      if(type !== 'gpa' && type !== 'order_cnt') throw "정렬 옵션이 잘못되었습니다.";
+
+      rest = await Restaurant.findAll({
+        where: {
+          [Op.and]: cond
+        },
+        order: [ [type, 'DESC'] ]
+      });
+    }
+
     res.status(200).json({
       success: true,
       restaurant: rest
@@ -18,9 +46,35 @@ exports.allRestaurants = async (req, res) => {
 
 exports.restaurantByCategory = async (req, res) => {
   const category = req.params.ctg;
+  const type = req.query.t;
+  const fee = req.query.f;
+  const min_price = req.query.m;
 
   try {
-    const rest = await Restaurant.findAll({ where: { tagCategoryCategory: category } });
+    let rest;
+    const cond = [];
+    cond.push({ tagCategoryCategory: category });
+
+    if(fee) cond.push({ deliveryFee: { [Op.lte]: fee } });
+    if(min_price) cond.push({ minOrderPrice: { [Op.lte]: min_price } });
+
+    if(type === undefined) {
+      rest = await Restaurant.findAll({
+        where: {
+          [Op.and]: cond
+        }
+      });
+    } else {
+      if(type !== 'gpa' && type !== 'order_cnt') throw "정렬 옵션이 잘못되었습니다.";
+
+      rest = await Restaurant.findAll({
+        where: {
+          [Op.and]: cond
+        },
+        order: [ [type, 'DESC'] ]
+      });
+    }
+    
     res.status(200).json({
       success: true,
       restaurant: rest
@@ -32,5 +86,3 @@ exports.restaurantByCategory = async (req, res) => {
     });
   }
 };
-
-// exports.
