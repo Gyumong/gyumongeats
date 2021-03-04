061b3489ce278e7912282636d0836affd749f236
Gyumong <rlaalsrb3559@naver.com>
2021-03-05 02:55:13 +0900

    MenuCard Styling

---
+++ front/components/Cart/MenuCard.js
@@ -0,0 +1,67 @@
+/** @format */
+
+import styled from "styled-components";
+import { Modal } from "antd";
+export const CartMenuCardBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const CartMenuCardTitle = styled.div`
+  h2 {
+    padding: 0.5rem 0 0.5rem 1rem;
+    font-weight: bold;
+    font-size: 1rem;
+    margin: 0;
+  }
+  border-bottom: 1px solid #c5c5c3;
+`;
+
+export const CartMenuCard = styled.div`
+  padding: 0.5rem 1rem;
+`;
+
+export const MenuTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 0.9rem;
+  margin-bottom: 0.2rem;
+`;
+
+export const MenuDesc = styled.div`
+  width: 55vw;
+  color: #9d9d9e;
+  font-size: 0.8rem;
+  line-height: 0.9rem;
+  margin-bottom: 0.2rem;
+`;
+
+export const MenuPrice = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 0.9rem;
+`;
+
+export const QuantitySelect = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 20vw;
+  height: 1.5rem;
+  padding: 0.2rem 0.4rem;
+  border: 1px solid #c5c5c3;
+  border-radius: 5px;
+  p {
+    font-size: 0.9rem;
+    margin: 0;
+  }
+`;
+
+export const QuantityModal = styled(Modal)`
+  display: flex;
+  flex-direction: column;
+  p {
+    margin: 0;
+  }
+`;
--- front/pages/cart.js
+++ front/pages/cart.js
@@ -1,19 +1,127 @@
 /** @format */
 
-import React, { useEffect } from "react";
-import AppLayout from "../components/AppLayout";
-import { useSelector } from "react-redux";
-import Router from "next/router";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import router from "next/router";
+import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import wrapper from "../store/configureStore";
+import { END } from "redux-saga";
+import axios from "axios";
+import { Header, ExitButton } from "../components/Cart/Header";
+import {
+  CartMenuCardBlock,
+  CartMenuCardTitle,
+  CartMenuCard,
+  MenuTitle,
+  MenuDesc,
+  MenuPrice,
+  QuantitySelect,
+  QuantityModal,
+} from "../components/Cart/MenuCard";
+import {
+  CloseOutlined,
+  ShoppingCartOutlined,
+  CaretDownOutlined,
+} from "@ant-design/icons";
+import styled from "styled-components";
 
+const CartBlock = styled.div`
+  padding-top: 5vh;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 const Cart = () => {
   const { me } = useSelector((state) => state.user);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  useEffect(() => {
-    if (!me) {
-      Router.push("/login");
-    }
-  }, [me]);
-  return <AppLayout>장바구니</AppLayout>;
+  const showModal = useCallback(() => {
+    setIsModalVisible(true);
+  }, [isModalVisible]);
+
+  const handleOk = useCallback(() => {
+    setIsModalVisible(false);
+  }, [isModalVisible]);
+  const ExitCart = useCallback(() => {
+    router.push("/");
+  }, []);
+  if (!me) {
+    return router.push("/login");
+  }
+
+  // <ShoppingCartOutlined style={{ fontSize: "100px" }} />
+  // <h3>장바구니가 비어있습니다.</h3>
+  return (
+    <>
+      <Header>
+        <ExitButton onClick={ExitCart} icon={<CloseOutlined />} />
+        <h1>카트</h1>
+      </Header>
+      <CartBlock>
+        <CartMenuCardBlock>
+          <CartMenuCardTitle>
+            <h2>바스버거 문정역점</h2>
+          </CartMenuCardTitle>
+          <CartMenuCard>
+            <MenuTitle>
+              칠면조버거 세트 <CloseOutlined />
+            </MenuTitle>
+            <MenuDesc>
+              치즈베이컨프라이로 변경 (+500원),스프라이트(355ml로)변경
+            </MenuDesc>
+            <MenuPrice>
+              23,400원
+              <QuantitySelect onClick={showModal}>
+                <p>1</p>
+                <CaretDownOutlined />
+                <QuantityModal
+                  visible={isModalVisible}
+                  cancelButtonProps={true}
+                  footer={false}
+                  closable={false}
+                  width={"80%"}
+                  bodyStyle={{ padding: 0 }}
+                >
+                  <p>1</p>
+                  <p>2</p>
+                  <p>3</p>
+                  <p>4</p>
+                  <p>5</p>
+                </QuantityModal>
+              </QuantitySelect>
+            </MenuPrice>
+          </CartMenuCard>
+        </CartMenuCardBlock>
+      </CartBlock>
+    </>
+  );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    const cookie = context.req ? context.req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+      const { accessToken } = await axios
+        .get("/auth/reissue", {
+          withCredentials: true,
+        })
+        .then((res) => res.data);
+      console.log("acctoken", accessToken);
+      if (accessToken) {
+        axios.defaults.headers.common[
+          "x-access-token"
+        ] = await `${accessToken}`;
+        context.store.dispatch({
+          type: LOAD_MY_INFO_REQUEST,
+        });
+      }
+    }
+
+    context.store.dispatch(END);
+    await context.store.sagaTask.toPromise();
+  }
+);
 
 export default Cart;
