9b4d596572fc4daa09c69ac2ca7cae541f755336
Jiho Park <jiho5993@naver.com>
2021-03-29 22:07:52 +0900

    즐겨찾기 관련 API 작성

--- back/routes/index.js
+++ back/routes/index.js
@@ -4,11 +4,13 @@ const store = require('./api/store/store.index');
 const cart = require('./api/cart/cart.index');
 const order = require('./api/order/order.index');
 const review = require('./api/review/review.index');
+const bookmark = require('./api/bookmark/bookmark.index');
 
 router.use('/auth', auth);
 router.use('/store', store);
 router.use('/cart', cart);
 router.use('/order', order);
 router.use('/review', review);
+router.use('/bookmark', bookmark);
 
 module.exports = router;
