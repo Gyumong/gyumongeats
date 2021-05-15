c8963e41273de756075f29ddec726e896808f6d0
Jiho Park <jiho5993@naver.com>
2021-05-16 01:20:16 +0900

    배포용 미들웨어 설정

--- back/app.js
+++ back/app.js
@@ -21,7 +21,11 @@ sequelize
     console.error(err);
   });
 
-app.use(logger("dev"));
+if(process.env.NODE_ENV === 'production') {
+  app.use(logger("combined"));
+} else if(process.env.NODE_ENV === 'development') {
+  app.use(logger("dev"));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
