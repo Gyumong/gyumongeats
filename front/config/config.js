65a8f8a1b17e2cdc8634cd4df5fef85c9cf3301b
Gyumong <rlaalsrb3559@naver.com>
2021-05-21 23:51:44 +0900

    backUrl 변경

--- front/config/config.js
+++ front/config/config.js
@@ -1,4 +1,3 @@
 /** @format */
 
-export const backUrl =
-  "http://gyumongeats-env-1.eba-n3bf4dyh.ap-northeast-2.elasticbeanstalk.com";
+export const backUrl = "http://www.api.gyumongeats.site/api";
